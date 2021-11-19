09ce3b209602e6d01a75bd8125457dda916a3c84
Toni Schmidbauer <toni@stderr.at>
2021-11-19 15:16:22 +0100

    and now it looks like gnunn's repo again
    he obviously knows what he's doing :-)

--- 00-deploy-gitops.sh
+++ 00-deploy-gitops.sh
@@ -2,12 +2,35 @@
 
 set -euf -o pipefail
 
+OVERLAY=${1:-default}
+KUSTOMIZE="/usr/bin/env kustomize"
+
 function error() {
     echo "$1"
 
     exit 1
 }
 
-/usr/bin/env kustomize 2>&1 >/dev/null || error "Could not execute kustomize binary!"
+function deploy() {
+    $KUSTOMIZE 2>&1 >/dev/null || error "Could not execute kustomize binary!"
+    $KUSTOMIZE build bootstrap/gitops/overlays/"$OVERLAY" | oc apply -f -
+}
 
-/usr/bin/env kustomize build bootstrap/gitops/base/ | oc apply -f -
+if [ "$OVERLAY" == "default" ]; then
+
+    cat<<EOF
+This will bootstrap the GitOps operator with the "$OVERLAY" overlay.
+
+If this is not what you want you can specify a different overlay via
+$0 <overlay name>
+
+EOF
+
+    echo -e "Currently this repo supports the following overlays:\n"
+    ls -1 bootstrap/gitops/overlays/
+
+    echo -e "\nHit CTRL+C now to specify a different overlay than '$OVERLAY'"
+    sleep 5
+fi
+
+deploy
---
+++ bootstrap/gitops/overlays/cluster-admin/README.md
@@ -0,0 +1,8 @@
+# GitOps with cluster admin
+
+This overlay grants cluster-admin privileges to
+*openshift-gitops-argocd-application-controller* service account.
+
+This is not how the cluster GitOps instance is intented to be
+used. But it's the lazy way of deploying everything that follows from
+here.
