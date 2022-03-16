e3967ea3e83ccfdc695fbbf2e658d3a9a3cef96c
Toni Schmidbauer <toni@stderr.at>
2022-03-16 17:40:50 +0100

    work on bootstrap script

--- 00-deploy-gitops.sh
+++ 00-deploy-gitops.sh
@@ -4,6 +4,11 @@ set -euf -o pipefail
 
 OVERLAY=${1:-default}
 KUSTOMIZE="/usr/bin/env kustomize"
+GITOPSNS="openshift-gitops"
+
+RED=$(tput setaf 1)
+NC=$(tput sgr0)
+
 
 function error() {
     echo "$1"
@@ -12,18 +17,21 @@ function error() {
 }
 
 function deploy() {
-    $KUSTOMIZE 2>&1 >/dev/null || error "Could not execute kustomize binary!"
+    $KUSTOMIZE >/dev/null 2>&1 || error "Could not execute kustomize binary!"
     $KUSTOMIZE build bootstrap/gitops/overlays/"$OVERLAY" | oc apply -f -
 }
 
+function wait_for_namespace() {
+    printf "Waiting for ${GITOPSNS} namespace...\n"
+    while ! oc get ns | grep ${GITOPSNS}x >/dev/null; do
+	sleep 3
+    done
+}
+
 function wait_for_route() {
-    echo "Waiting for openshift-gitops route..."
+    wait_for_namespace
 
-    # XXX actually we could check if the route is created and the status but
-    # XXX -ETOOLAZY
-    sleep 5
-
-    GITOPS_HOST=$(oc get route -n openshift-gitops openshift-gitops-server -o jsonpath="{.spec.host}")
+    GITOPS_HOST=$(oc get route -n ${GITOPSNS} openshift-gitops-server -o jsonpath="{.spec.host}")
     GITOPS_URL="https://${GITOPS_HOST}"
     echo "Go to ${GITOPS_URL} and log in with your openshift credentials to start using GitOps!"
 }
@@ -31,7 +39,7 @@ function wait_for_route() {
 if [ "$OVERLAY" == "default" ]; then
 
     cat<<EOF
-This will bootstrap the GitOps operator with the "$OVERLAY" overlay.
+This will bootstrap the GitOps operator with the ${RED}${OVERLAY}${NC} overlay.
 
 If this is not what you want you can specify a different overlay via
 $0 <overlay name>
