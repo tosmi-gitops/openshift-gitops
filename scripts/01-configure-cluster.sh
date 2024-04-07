469017215da0cad428bb7559af4f101e632cac64
Toni Schmidbauer <toni@stderr.at>
2024-04-07 11:53:09 +0200

    check the current dir before running kustomize

--- scripts/01-configure-cluster.sh
+++ scripts/01-configure-cluster.sh
@@ -2,6 +2,8 @@
 
 set -euf -o pipefail
 
+CURRENT_DIR=$(pwd)
+
 OVERLAY=${1:-NONE}
 
 function error() {
@@ -14,6 +16,12 @@ function usage() {
     cat <<EOF
 
 EOF
+}
+
+function check_directory() {
+    DIRNAME=$(basename "$CURRENT_DIR")
+
+    [ "$DIRNAME" != "openshift-gitops" ] && error "Please go to the base directory of the GitOps repository!"
 }
 
 /usr/bin/env kustomize >/dev/null 2>&1 || error "Could not execute kustomize binary!"
