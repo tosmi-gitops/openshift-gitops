b95e5cdc9a421aad5cac04d92a91d0c2bb3a0225
Toni Schmidbauer <toni@stderr.at>
2025-11-07 17:09:09 +0100

    fix namespace for oc delete pod
    make output more conform to other scripts in this repo

--- scripts/02-restore-sealed-secrets.sh
+++ scripts/02-restore-sealed-secrets.sh
@@ -1,17 +1,22 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # shellcheck disable=SC3040
 set -euf -o pipefail
 
 GPG_CMD="gpg"
-OC_CMD="oc"
+OC="oc"
 CURRENT_DIR=$(pwd)
 
 SECRET="etc/tntinfra-sealed-secrets.yaml.gpg"
 
-error() {
-    echo "$1"
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+NORMAL=$(tput sgr0)
+NC=$(tput sgr0)
 
+error() {
+    echo "${RED}${1}${NC}"
     exit 1
 }
 
@@ -23,10 +28,12 @@ EOF
 
 verify() {
     command -v "$GPG_CMD" > /dev/null 2>&1 || error "gpg command line tool not found"
-    command -v "$OC_CMD" > /dev/null 2>&1 || error "oc command line tool not found"
+    command -v "$OC" > /dev/null 2>&1 || error "oc command line tool not found"
+
+    echo "Checking for sealed-secrets namespace..."
+    $OC get ns sealed-secrets >/dev/null 2>&1 || error "Checking for ${GREEN}sealed-secrets${NC} namespace failed, is the secrets controller deployed?"
 
     [ -f "$SECRET" ] || error "Secret not found: $SECRET "
-
 
     DIRNAME=$(basename "$CURRENT_DIR")
     [ "$DIRNAME" != "openshift-gitops" ] && error "Please go to the base directory of the GitOps repository!"
@@ -35,8 +42,8 @@ verify() {
 }
 
 restore() {
-    "$GPG_CMD" -d "$SECRET" | "$OC_CMD" apply -f -
-    "$OC_CMD" delete pod -l app.kubernetes.io/name=sealed-secrets
+    "$GPG_CMD" -d "$SECRET" | "$OC" apply -n sealed-secrets -f -
+    "$OC" delete pod -n sealed-secrets -l app.kubernetes.io/name=sealed-secrets
 }
 
 verify
